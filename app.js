a6a83f56e2f1ede4d50343b95a8a77eead5ec034
Wanderson Oliveira <testepedroca@gmail.com>
2021-02-08 18:45:09 -0300

    utilizando axios

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
+
 var button = document.querySelector('#app button')
 
-button.addEventListener('click', () => {
-  var xhr = new XMLHttpRequest()
-
-  xhr.open('GET', 'https://api.github.com/users')
-  xhr.send()
-
-  xhr.onreadystatechange = () => {
-    if(xhr.readyState === 4) {
-      var data = JSON.parse(xhr.responseText)
-      console.log(data[2].login)
-    }
-  }
+button.addEventListener('click',() => {
+  axios.get('https://api.github.com/users')
+    .then((response) => {
+      var data = response.data
+      console.log(data[0])
+    })
+    .catch((error) => {
+      console.log(error)
+    })
 })
